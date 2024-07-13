5e7135781cbd50a0f2fdc7ee2d3ec176d4afd281
DiaDoesDastardlyDeeds <borg.stuff@gmail.com>
2024-07-13 12:40:31 -0600

    Readded city cubes, discovered new rendering issues
    The cubes keep rendering on top of one another, most likely an issue with processing the depth

--- obj/Debug/net8.0-windows/AutoCivBuilder.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/AutoCivBuilder.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AutoCivBuilder")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b2bff840ed9de042ab9cf6a50df8329cd3e674e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5caf1e7da57fe199db70821541c856cff5591994")]
 [assembly: System.Reflection.AssemblyProductAttribute("AutoCivBuilder")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AutoCivBuilder")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
